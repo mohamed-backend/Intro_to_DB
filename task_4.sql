9221d479507147ac1f57099948ab1d602aa510b7
mikwall <charchaoui.mohamed77@gmail.com>
2025-02-02 22:35:26 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,2 @@
--- Use the database passed as an argument
-USE DATABASE;
-
--- Query the INFORMATION_SCHEMA to get details about the columns in the 'books' table
-SELECT COLUMN_NAME AS 'Column Name',
-       COLUMN_TYPE AS 'Column Type',
-       IS_NULLABLE AS 'Is Nullable',
-       COLUMN_DEFAULT AS 'Default Value',
-       EXTRA AS 'Extra Information'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
 -- This script lists the columns in the books table using SHOW CREATE TABLE
 SHOW CREATE TABLE books;
-
