8d9966664622b4a9ff8e0b7e6f47120daa082e98
mikwall <charchaoui.mohamed77@gmail.com>
2025-02-02 23:08:41 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,19 +1,6 @@
--- Use the provided database (this will be passed as an argument when executing the script)
-USE DATABASE;
-
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES 
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
--- Ensure the customer with customer_id=2 is inserted
-SELECT * FROM customers WHERE customer_id = 2;
-
--- Ensure the customer with customer_id=3 is inserted
-SELECT * FROM customers WHERE customer_id = 3;
-
--- Ensure the customer with customer_id=4 is inserted
-SELECT * FROM customers WHERE customer_id = 4;
-mysql -u root -p < task_6.sql alx_book_store
