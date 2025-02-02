974ac6a519bf9168489073740c952f8b412d6f3e
mikwall <charchaoui.mohamed77@gmail.com>
2025-02-02 22:19:12 +0100

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,6 +1,13 @@
--- Use the provided database (this will be passed as an argument when executing the script)
--- Use the database specified by the user (e.g., alx_book_store)
-USE DATABASE;
+-- Check if the current database is 'alx_book_store'
+IF DATABASE() != 'alx_book_store' THEN
+    -- If the database is not 'alx_book_store', show an error message
+    SELECT 'Error: You are not using the alx_book_store database.' AS error_message;
+    -- Optionally, exit the script after showing the error
+    LEAVE;
+ELSE
+    -- Use the correct database
+    USE alx_book_store;
 
--- Show all tables in the current database
-SHOW TABLES;
+    -- Show all tables in the 'alx_book_store' database
+    SHOW TABLES;
+END IF;
