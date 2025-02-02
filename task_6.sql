22f41e26dbfb334b178b21be036ad2d28d8014c8
mikwall <charchaoui.mohamed77@gmail.com>
2025-02-02 22:38:47 +0100

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -7,3 +7,12 @@ VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+-- Ensure the customer with customer_id=2 is inserted
+SELECT * FROM customers WHERE customer_id = 2;
+
+-- Ensure the customer with customer_id=3 is inserted
+SELECT * FROM customers WHERE customer_id = 3;
+
+-- Ensure the customer with customer_id=4 is inserted
+SELECT * FROM customers WHERE customer_id = 4;
