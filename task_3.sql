8c2b9436243e927425387b871880572a2d4fa454
mikwall <charchaoui.mohamed77@gmail.com>
2025-02-02 22:17:50 +0100

    Create task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+-- Use the provided database (this will be passed as an argument when executing the script)
+-- Use the database specified by the user (e.g., alx_book_store)
+USE DATABASE;
+
+-- Show all tables in the current database
+SHOW TABLES;
